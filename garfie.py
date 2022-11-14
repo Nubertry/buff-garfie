90ee8d28c3cf685ef7a2363b210c822d0fc8f4af
Bertry <110958802+Nubertry@users.noreply.github.com>
2022-11-14 18:08:59 -0500

    Add files via upload

---
+++ garfie.py
@@ -0,0 +1,21 @@
+from PIL import Image
+
+#...
+
+img1 = Image.open('GarfImages/garf1.jpg')
+img2 = Image.open('GarfImages/garf2.jpg')
+img3 = Image.open('GarfImages/garf3.jpg')
+img4 = Image.open('GarfImages/garf4.jpg')
+
+img1.show()
+img2.show()
+img3.show()
+img4.show()
+img1.show()
+img2.show()
+img3.show()
+img4.show()
+img1.show()
+img2.show()
+img3.show()
+img4.show()
